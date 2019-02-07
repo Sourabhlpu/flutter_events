2c4b99790f1041176012bcad159ffd727c781704
sourabh <sourabh.pal@appinventiv.com>
2019-02-08 02:28:41 +0530

    implemented the indicators as well. A lot of technical debt. Need to optimize code

--- lib/main.dart
+++ lib/main.dart
@@ -15,44 +15,218 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  State createState() => MyHomePageState();
+}
+
+class MyHomePageState extends State<MyHomePage> {
+  final PageController _pagecontroller = new PageController(initialPage: 0);
+  int _currentPage = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: DefaultTabController(
-          length: 3,
-          child: TabBarView(children: [
-            Container(
+        body: Stack(
+      children: <Widget>[
+        PageView(
+            controller: _pagecontroller,
+            onPageChanged: (int i) {
+              setState(() {
+                _currentPage = i;
+              });
+            },
+            children: [
+              Container(
                 color: Colors.white,
-                width: double.infinity,
-                height: double.infinity,
-                child: Align(
-                  alignment: Alignment.topCenter,
-                  child: Padding(
-                    padding: EdgeInsets.only(top: 20.0),
-                    child: Image.asset('images/discover.png'),
+                child: Column(children: [
+                  Align(
+                    alignment: Alignment.topCenter,
+                    child: Padding(
+                      padding: EdgeInsets.only(top: 50.0),
+                      child: Image.asset(
+                        'images/discover.png',
+                      ),
+                    ),
                   ),
-                )),
-            Container(
-              color: Colors.white,
-              width: double.infinity,
-              height: double.infinity,
-              child: Align(
-                alignment: Alignment.topCenter,
-                child: Padding(padding: EdgeInsets.only(top: 20.0),
-                child: Image.asset('images/create.png'),),
-              )
-            ),
-            Container(
-              color: Colors.white,
-              width: double.infinity,
-              height: double.infinity,
-              child: Align(
-                alignment: Alignment.topCenter,
-                child: Padding(padding: EdgeInsets.only(top: 20.0), child: Image.asset('images/book.png'),),
-              )
-            ),
-          ])),
+                  SizedBox(width: 0.0, height: 80.0),
+                  Align(
+                    alignment: Alignment.bottomLeft,
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 16.0),
+                      child: Text(
+                        'Discover Event',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.w400),
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 0.0, height: 25.0),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Text(
+                      'There are many variations of passages of Lorem ipsum available',
+                      style:
+                          TextStyle(fontSize: 14, fontWeight: FontWeight.w400),
+                    ),
+                  )
+                ]),
+              ),
+              Container(
+                color: Colors.white,
+                child: Column(children: [
+                  Align(
+                    alignment: Alignment.topCenter,
+                    child: Padding(
+                      padding: EdgeInsets.only(top: 50.0),
+                      child: Image.asset(
+                        'images/create.png',
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 0.0, height: 80.0),
+                  Align(
+                    alignment: Alignment.bottomLeft,
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 16.0),
+                      child: Text(
+                        'Create New Event',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.w400),
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 0.0, height: 25.0),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Text(
+                      'There are many variations of passages of Lorem ipsum available',
+                      style:
+                          TextStyle(fontSize: 14, fontWeight: FontWeight.w400),
+                    ),
+                  )
+                ]),
+              ),
+              Container(
+                color: Colors.white,
+                child: Column(children: [
+                  Align(
+                    alignment: Alignment.topCenter,
+                    child: Padding(
+                      padding: EdgeInsets.only(top: 50.0),
+                      child: Image.asset(
+                        'images/book.png',
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 0.0, height: 80.0),
+                  Align(
+                    alignment: Alignment.bottomLeft,
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 16.0),
+                      child: Text(
+                        'Book Event',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.w400),
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 0.0, height: 25.0),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Text(
+                      'There are many variations of passages of Lorem ipsum available',
+                      style:
+                          TextStyle(fontSize: 14, fontWeight: FontWeight.w400),
+                    ),
+                  )
+                ]),
+              ),
+            ]),
+        Align(
+            alignment: Alignment.bottomCenter,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      width: 10,
+                      height: 10,
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: _currentPage == 0
+                              ? const Color(0xFFED5D66)
+                              : Colors.transparent,
+                          border: Border.all(color: Colors.grey[500])),
+                    ),
+                    SizedBox(
+                      width: 4,
+                    ),
+                    Container(
+                      width: 10,
+                      height: 10,
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: _currentPage == 1
+                              ? Color(0xFFED5D66)
+                              : Colors.transparent,
+                          border: Border.all(color: Colors.grey[500])),
+                    ),
+                    SizedBox(
+                      width: 4,
+                    ),
+                    Container(
+                      width: 10,
+                      height: 10,
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: _currentPage == 2
+                              ? Color(0xFFED5D66)
+                              : Colors.transparent,
+                          border: Border.all(color: Colors.grey[500])),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  width: 0,
+                  height: 8.0,
+                ),
+                PrimaryGradientButton('Get Started')
+              ],
+            )),
+
+        /*Align(
+            alignment: Alignment.bottomCenter,
+            child: PrimaryGradientButton('Get Started')),*/
+      ],
+    ));
+  }
+}
+
+class PrimaryGradientButton extends StatelessWidget {
+  final String btnText;
+
+  PrimaryGradientButton(this.btnText);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.9,
+      margin: EdgeInsets.only(bottom: 8.0),
+      decoration: BoxDecoration(
+          borderRadius: new BorderRadius.circular(30.0),
+          gradient: LinearGradient(
+              colors: [const Color(0xFFDF69DD), const Color(0xFFED5D66)],
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight)),
+      child: FlatButton(
+        onPressed: () {
+          print('get started');
+        },
+        child: Text(btnText),
+        textColor: Colors.white,
+        padding: EdgeInsets.symmetric(vertical: 15.0),
+      ),
     );
   }
 }
